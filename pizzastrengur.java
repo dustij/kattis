a5e632013c67e9051d72f907770a0b56d1b8a35d
dustij <knightlybyte@gmail.com>
2024-11-21 14:35:19 -0600

    pizzastrengur javadoc

--- pizzastrengur.java
+++ pizzastrengur.java
@@ -1,28 +1,105 @@
+/**
+ * Today is game day at Tommi's Pizzas, so Georg and his friends are going to order some pizzas.
+ * The game starts with the employees at Tommi's Pizzas choosing a secret passcode, consisting only of the letters {"P", "I", "Z", "A"},
+ * and telling the customer the length of the passcode. If Georg and his friends manage to guess the passcode in at most m attempts,
+ * they get their pizza for free!
+ * 
+ * Georg and his friends may pick words shorter than the passcode as guesses, in which case they will be told whether the guess is a prefix of the passcode.
+ * The goal is to help Georg and his friends in guessing the passcode, so they can get their pizza for free.
+ * 
+ * A word A is considered a prefix of a word B if B starts with A. For example, "PIZ" is a prefix of "PIZZA", but "ZZA" is not a prefix of "PIZZA".
+ * 
+ * <h2>Interactivity</h2>
+ * This is an interactive problem. Your solution will be tested against an interactive judge which reads the output of your solution
+ * and prints the input your solution receives. The interaction follows these rules:
+ * 
+ * <ol>
+ *   <li>The judge starts by printing an integer n (1 <= n <= 10^4), which represents the length of the passcode S. The passcode only contains the letters {"P", "I", "Z", "A"}.</li>
+ *   <li>Next, your solution must output a guess string P which must contain at least 1 letter and at most n letters.</li>
+ *   <li>After this, the judge will respond as follows:</li>
+ *   <ul>
+ *     <li>If P is not a prefix of S, the judge will print 0.</li>
+ *     <li>If P is a prefix of S, the judge will print 1.</li>
+ *     <li>If P is equal to the passcode, the judge will print 2 and your program should stop guessing.</li>
+ *   </ul>
+ * </ol>
+/**
+ * Today is game day at Tommi's Pizzas, so Georg and his friends are going to order some pizzas.
+ * The game starts with the employees at Tommi's Pizzas choosing a secret passcode, consisting only of the letters {"P", "I", "Z", "A"},
+ * and telling the customer the length of the passcode. If Georg and his friends manage to guess the passcode in at most m attempts,
+ * they get their pizza for free!
+ * 
+ * Georg and his friends may pick words shorter than the passcode as guesses, in which case they will be told whether the guess is a prefix of the passcode.
+ * The goal is to help Georg and his friends in guessing the passcode, so they can get their pizza for free.
+ * 
+ * A word A is considered a prefix of a word B if B starts with A. For example, "PIZ" is a prefix of "PIZZA", but "ZZA" is not a prefix of "PIZZA".
+ * 
+ * Interactivity
+ * This is an interactive problem. Your solution will be tested against an interactive judge which reads the output of your solution
+ * and prints the input your solution receives. The interaction follows these rules:
+ * 
+ * The judge starts by printing an integer n (1 <= n <= 10^4), which represents the length of the passcode S. The passcode only contains the letters {"P", "I", "Z", "A"}.
+ * Next, your solution must output a guess string P which must contain at least 1 letter and at most n letters.
+ * After this, the judge will respond as follows:
+ * - If P is not a prefix of S, the judge will print 0.
+ * - If P is a prefix of S, the judge will print 1.
+ * - If P is equal to the passcode, the judge will print 2 and your program should stop guessing.
+ * 
+ * Make sure to flush after each guess. For example:
+ * - In Python: print(..., flush=True)
+ * - In C++: cout << ... << endl;
+ * - In Java: System.out.flush();
+ * 
+ * The problem comes with a testing tool to help test your solution.
+ * 
+ * Scoring
+ * Group  | Points | Constraints
+ * -------|--------|------------
+ * 1      | 10     | 1 <= n <= 10^3, m = 4 * n
+ * 2      | 25     | 1 <= n <= 10^3, m = 3 * n + 1
+ * 3      | 25     | n = 10^3, m = 2.75 * n
+ * 4      | 40     | n = 10^3, m = 2.45 * n
+ */
+
+
 import java.util.Scanner;
 
-public class pizzastrengur
-{
+public class pizzastrengur {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
+        Scanner in = new Scanner(System.in);
         StringBuilder sb = new StringBuilder();
         char[] choices = {'P', 'I', 'Z', 'A'};
-        int length = input.nextInt();
+
+        int length = in.nextInt();
         for (int i = 0; i < length; i++) {
+            int guessCount = 0; // only allow 3 guess per character, don't need to guess the last
+                                // because obvisouly that is correct, just move on to next prefix
             for (char choice : choices) {
-                String guess = sb.toString() + choice;
-                System.out.println(guess);
-                int res = input.nextInt();
-                // stop guessing
-                if (res == 2) {
-                    return;
-                }
-                // is prefix
-                if (res == 1) {
+                if (guessCount == 3) {
                     sb.append(choice);
+                    if (sb.length() == length) {
+                        System.out.println(sb.toString());
+                        return;
+                    }
                     break;
+                }
+
+                sb.append(choice);
+                System.out.println(sb.toString());
+                int res = in.nextInt(); 
+                guessCount++;
+
+                if (res == 2) {
+                    // stop guessing
+                    return;
+                } else if (res == 1) {
+                    // valid prefix
+                    break;
+                } else {
+                    // invalid prefix
+                    sb.deleteCharAt(sb.length() - 1);
                 }
             }
         }
-        input.close();
     }
 }
--- prozor.java
+++ prozor.java
@@ -4,7 +4,6 @@
 
 import java.util.Scanner;
 import java.util.stream.Stream;
-import java.util.List;
 
 public class prozor {
     public static void main(String[] args) {
