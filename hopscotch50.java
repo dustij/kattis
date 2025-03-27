9a76ade760b4ec72a90fd04927aa74fd6140a3f5
dustij <dev.dusti@gmail.com>
2025-03-27 12:24:23 -0500

    start implement dijkstras algo

--- hopscotch50.java
+++ hopscotch50.java
@@ -1,35 +1,56 @@
 import java.util.Scanner;
-import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.Stack;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.PriorityQueue;
+
+
 
 public class hopscotch50 {
+
+    static class Node {
+        int x;
+        int y;
+        boolean visited;
+        int distance;
+
+        Node(int x, int y) {
+            this.x = x;
+            this.y = y;
+            visited = false;
+            distance = Integer.MAX_VALUE;
+        }
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         int n = sc.nextInt();
         int k = sc.nextInt();
 
+        // Keep track of shortest distance - this is the output for the problem
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
 
+        // Manage all nodes for dijkstras algorithm
+        LinkedList<Stack<Node>> stacksList = new LinkedList<>();
 
-        ArrayList<ArrayList<Integer[]>> coordGrid = new ArrayList<>();
-        for (int i = 0; i < k; i++) {
-            coordGrid.add(new ArrayList<>());
-
-        }
-
+        // Check path is possible
         boolean[] exists = new boolean[k];
 
-        int[][] arr = new int[n][n];
+        // Poulate
+        for (int i = 0; i < k; i++) {
+            stacksList.add(new Stack<Node>());
+        }
+
         for (int row = 0; row < n; row++) {
             for (int col = 0; col < n; col++) {
                 int num = sc.nextInt();
-                arr[row][col] = num;
                 exists[num - 1] = true;
-                coordGrid.get(num - 1).add(new Integer[] {row, col});
+                stacksList.get(num - 1).add(new Node(row, col));
             }
         }
 
-
+        // Early out
         for (boolean b : exists) {
             if (!b) {
                 System.out.println(-1);
@@ -37,68 +58,82 @@ public class hopscotch50 {
             }
         }
 
-        int acc = 0;
-        int min = Integer.MAX_VALUE;
-        Integer[] bestP = null;
+        // printQueue(stacksList);
 
-        // init bestP from k-2 -> k-1
-        for (var p1 : coordGrid.get(k - 2)) {
-            for (var p2 : coordGrid.get(k - 1)) {
-                var d = getDistance(p1, p2);
-                if (d < min) {
-                    min = d;
-                    bestP = p1;
+        // Get shortest distance for each starting point to each ending point
+        var startStack = stacksList.poll();
+        var endStack = stacksList.getLast();
+
+        // printQueue(stacksList);
+
+        while (!startStack.empty()) {
+            // Temporary list to preserve base nodes
+            LinkedList<Stack<Node>> tempList = new LinkedList<>();
+            for (var stack : stacksList) {
+                var newStack = new Stack<Node>();
+                for (var node : stack) {
+                    newStack.add(new Node(node.x, node.y));
                 }
+                tempList.add(stack);
+            }
+
+            Node startNode = startStack.pop();
+
+            while (!endStack.empty()) {
+                Node endNode = endStack.pop();
+
+                // Solve and save min distance
+                minHeap.offer(solve(startNode, stacksList.subList(0, stacksList.size() - 1), endNode));
+
+            }
+
+            // Restore list back to base
+            for (int i = 0; i < tempList.size(); i++) {
+                stacksList.set(i, tempList.get(i));
+            }
+
+
+        }
+
+        // printQueue(stacksList);
+
+    }
+
+    public static Integer solve(Node startNode, List<Stack<Node>> nodesBetween, Node endNode) {
+        // Dijkstras Algorithm
+        return 1;
+    }
+
+    public static void printQueue(LinkedList<Stack<Node>> queue) {
+        // ChatGPT generated - shhh...
+
+        System.out.println("--- Queue Contents ---");
+        for (int i = 0; i < queue.size(); i++) {
+            Stack<Node> stack = queue.get(i);
+            System.out.printf("Stack %d (size: %d):\n", i + 1, stack.size());
+
+            // Temporary stack to preserve original stack order
+            Stack<Node> tempStack = new Stack<>();
+            while (!stack.isEmpty()) {
+                Node node = stack.pop();
+                System.out.printf("  Node(x=%d, y=%d, visited=%b, distance=%d)\n",
+                        node.x, node.y, node.visited, node.distance);
+                tempStack.push(node);
+            }
+
+            // Restore the original stack
+            while (!tempStack.isEmpty()) {
+                stack.push(tempStack.pop());
             }
         }
-        acc += min;
-        //
-        for (int i = k - 3; i >= 0; i--) {
-            min = Integer.MAX_VALUE;
-            Integer[] nextP = null;
-            for (var p1 : coordGrid.get(i)) {
-                var d = getDistance(p1, bestP);
-                if (d < min) {
-                    nextP = p1;
-                    min = d;
-                }
-            }
-            bestP = nextP;
-            acc += min;
-        }
-
-        // find best last move
-        // for (int i = k; i >= 2; i--) {
-        // Integer[] nextP = null;
-
-        // for (var p1 : coordGrid.get(i - 2)) {
-        // if (bestP == null) {
-        // for (var p2 : coordGrid.get(i - 1)) {
-        // var d = getDistance(p1, p2);
-        // if (d < min) {
-        // min = d;
-        // bestP = p1;
-        // }
-        // }
-        // } else {
-        // var d = getDistance(p1, bestP);
-        // if (d < min) {
-        // min = d;
-        // nextP = p1;
-        // }
-        // }
-        // }
-
-        // bestP = nextP;
-        // acc += min;
-        // min = Integer.MAX_VALUE;
-        // }
-
-        System.out.println(acc);
-
+        System.out.println("----------------------");
     }
 
     public static int getDistance(Integer[] p1, Integer[] p2) {
         return Math.abs(p1[0] - p2[0]) + Math.abs(p1[1] - p2[1]);
+    }
+
+    public static int getDistance(Node p1, Node p2) {
+        return Math.abs(p1.x - p2.x) + Math.abs(p1.y - p2.y);
     }
 }
